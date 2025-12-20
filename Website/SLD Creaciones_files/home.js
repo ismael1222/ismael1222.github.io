52830b440703fecfd23d69501cbc32928a16d0ab
ismael1222 <holgaloper@gmail.com>
2025-12-20 20:21:01 -0300

    Four: Placeholder

---
+++ Website/SLD Creaciones_files/home.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let slides = document.querySelectorAll(".slide-container");
+    let current = 0;
+    
+    // el primero tiene q ser visible
+    if (slides.length > 0) {
+        slides[current].classList.add('active');
+    }
+    
+    setInterval(() => {
+        if (slides.length === 0) return;
+        
+        // inicia quitando pq ya se puso de antes
+        slides[current].classList.remove('active');
+        slides[current].classList.add('post');
+        
+        let previous = current;
+        
+        // next until max (0)
+        current++;
+		current=current%slides.length;
+        
+        // Activa el nuevo slide
+        setTimeout(() => {
+            slides[current].classList.add('active');
+        }, 10);
+        
+        // reiniciar salido
+        setTimeout(() => {
+            slides[previous].classList.remove('post');
+        }, 1000);
+        
+    }, 5000); // Aumenté a 4 segundos para dar tiempo a leer el texto
+});
