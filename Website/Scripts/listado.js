1f23da790911888ea69befbe2265a6d58b52d43f
ismael1222 <holgaloper@gmail.com>
2026-01-07 16:59:30 -0300

    Pre card enhancement

--- Website/Scripts/listado.js
+++ Website/Scripts/listado.js
@@ -9,22 +9,37 @@ function createCard(name, desc, url, text) {
 	card.appendChild(cardIcon);
 	card.appendChild(cardName);
 	card.appendChild(cardDesc);
-	card.appendChild(cardButon);
-
+	card.appendChild(cardButton);
+	console.log(name, desc, url, text);
 	card.classList.add("card");
 	cardIcon.classList.add("card-icon");
-	cardIconImage.src = url;
-	cardIconImage.alt = name;
-	cardName.innerText = name;
-	cardDesc.innerText = desc;
-	cardButton.href = text;
+	cardIconImage.src = url || "";
+	cardIconImage.alt = name || "unknownImageName";
+	cardName.innerText = name || "unknownName";
+	cardDesc.innerText = desc || "unknownDesc";
+	cardButton.href = text || "unknownHref";
 	cardButton.classList.add("boton");
+	cardButton.innerHTML = "Añadir al carrito";
 
 	return card;
 }
-fetch("./listado.json").then((response) => {
+/* fetch("./listado.json").then((response) => {
 	if (!response.ok) {
 		throw new Error(`HTTP error! Status: ${response.status}`);
 	}
 	console.log(response);
+});
+ */
+let responseJson = JSON.parse(
+	'[{"producto": "Jeans","texto":"whatsappTexto","descripcion":"descripcion","fotoUrl":"https://randommer.io/images/clothes/Jeans.webp"},{"producto":"Cargos","texto":"whatsappTexto","descripcion": "descripcion",		"fotoUrl": "https://randommer.io/images/clothes/Cargos.webp"	},	{		"producto": "Sandals",		"texto": "whatsappTexto",		"descripcion": "descripcion",		"fotoUrl": "https://randommer.io/images/clothes/Sandals.webp"	},	{		"producto": "Top hat",		"texto": "whatsappTexto",		"descripcion": "descripcion",		"fotoUrl": "https://randommer.io/images/clothes/Top%20Hat.webp"	},	{		"producto": "Flannel Shirt",		"texto": "whatsappTexto",		"descripcion": "descripcion",		"fotoUrl":"https://randommer.io/images/clothes/Flannel%20Shirt.webp"},{"producto":"Referee uniform","texto":"whatsappTexto","descripcion":"descripcion","fotoUrl":"https://randommer.io/images/clothes/Referee%20Uniform.webp"},{"producto":"Shirt","texto":"whatsappTexto","descripcion":"descripcion","fotoUrl":"https://randommer.io/images/clothes/Shirt.webp"}]',
+);
+
+responseJson.forEach((element) => {
+	console.log("//////////");
+	console.log(element);
+	console.log(element.producto);
+	let card = createCard(element.producto, element.descripcion, element.fotoUrl, element.texto);
+	console.log(Object.keys(element));
+	console.log(card);
+	document.querySelector(".cards-grid").appendChild(card);
 });
