777d6e6a7b62a7fb8f5b9145e40f4ad0ca833a1a
ismael1222 <holgaloper@gmail.com>
2026-01-07 15:42:48 -0300

    filesystem

--- Website/Scripts/home.js
+++ Website/Scripts/home.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
 	let slides = document.querySelectorAll(".slide-container");
 	let current = 0;
 
 	// el primero tiene q ser visible
 	if (slides.length > 0) {
-		slides[current].classList.add('active');
+		slides[current].classList.add("active");
 	}
 
 	setInterval(() => {
 		if (slides.length === 0) return;
 
 		// inicia quitando pq ya se puso de antes
-		slides[current].classList.remove('active');
-		slides[current].classList.add('post');
+		slides[current].classList.remove("active");
+		slides[current].classList.add("post");
 
 		let previous = current;
 
@@ -22,13 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		// Activa el nuevo slide
 		setTimeout(() => {
-			slides[current].classList.add('active');
+			slides[current].classList.add("active");
 		}, 10);
 
 		// reiniciar salido
 		setTimeout(() => {
-			slides[previous].classList.remove('post');
+			slides[previous].classList.remove("post");
 		}, 1000);
-
 	}, 5000); // 5s to better readability
 });
---
+++ Website/Scripts/listado.js
@@ -0,0 +1,30 @@
+function createCard(name, desc, url, text) {
+	var card = document.createElement("div");
+	var cardIcon = document.createElement("div");
+	var cardIconImage = document.createElement("img");
+	var cardName = document.createElement("h3");
+	var cardDesc = document.createElement("p");
+	var cardButton = document.createElement("a");
+	cardIcon.appendChild(cardIconImage);
+	card.appendChild(cardIcon);
+	card.appendChild(cardName);
+	card.appendChild(cardDesc);
+	card.appendChild(cardButon);
+
+	card.classList.add("card");
+	cardIcon.classList.add("card-icon");
+	cardIconImage.src = url;
+	cardIconImage.alt = name;
+	cardName.innerText = name;
+	cardDesc.innerText = desc;
+	cardButton.href = text;
+	cardButton.classList.add("boton");
+
+	return card;
+}
+fetch("./listado.json").then((response) => {
+	if (!response.ok) {
+		throw new Error(`HTTP error! Status: ${response.status}`);
+	}
+	console.log(response);
+});
